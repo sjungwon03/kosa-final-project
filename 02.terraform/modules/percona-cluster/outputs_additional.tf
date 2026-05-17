dbb4d914f834c9626f52cba98aa29d92a5316f4a
sjungwon03 <sjungwon03@gmail.com>
2026-05-17 22:46:14 +0900

    chore: Proxmox VM 모듈 및 Ansible 관련 파일 삭제

--- 02.terraform/modules/percona-cluster/outputs_additional.tf
+++
@@ -1,9 +0,0 @@
-output "proxysql_admin_url" {
-  description = "ProxySQL admin URL"
-  value       = "${split("/", module.proxysql[0].vm_ip)[0]}:6032"
-}
-
-output "mysql_connection_string_proxysql" {
-  description = "MySQL connection string via ProxySQL"
-  value       = "mysql -h ${var.internal_ip_prefix}.105 -P 6033 -u root -p"
-}
