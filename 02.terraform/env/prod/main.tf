7567768c1a61e6adad8acc0f99688dd14fc0f46b
yjj <yoojj0406@gmail.com>
2026-05-18 23:25:59 +0900

    refactor: prod main.tf 수정

--- 02.terraform/env/prod/main.tf
+++ 02.terraform/env/prod/main.tf
@@ -5,6 +5,24 @@ terraform {
       version = "~> 0.66"
     }
   }
+
+  backend "s3" {
+    # key는 02-run.sh에서 -backend-config로 주입 (서비스별 state 분리)
+    bucket = "terraform-state"
+    region = "us-east-1"
+
+    endpoints = {
+      s3 = "http://172.16.30.70:9000"
+    }
+    access_key                  = "kosa"
+    secret_key                  = "kosa1004"
+
+    skip_credentials_validation  = true
+    skip_metadata_api_check      = true
+    skip_region_validation       = true
+    skip_requesting_account_id   = true
+    use_path_style               = true
+  }
 }
 
 provider "proxmox" {
